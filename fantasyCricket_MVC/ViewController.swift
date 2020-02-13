818268c59ee86ed5dec535d7390813d675e3ad81
Praveen Kumar <praveen.peddachitti@gmail.com>
2020-02-13 14:24:09 +0530

    Initial Commit

---
+++ fantasyCricket_MVC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  fantasyCricket_MVC
+//
+//  Created by Adaps on 13/02/20.
+//  Copyright © 2020 CrazyTricks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
